d8f895df6739e91bb6abf5aa8a37984c04ebe34c
KamisP13 <kamila.carvalhop@gmail.com>
2023-09-25 16:26:28 -0300

    .

--- arquivo.js
+++ arquivo.js
@@ -1,20 +1,20 @@
-const taylorSwiftQuotes = [
-    "O que importa é a maneira como você se vê.",
-    "Lembre-se de ser você mesmo, porque ninguém pode fazer isso melhor do que você.",
-    "Nunca mude para ser aceito. Mude porque você quer evoluir e crescer.",
-    "A vida é muito curta para se preocupar com o que os outros pensam de você."
+const citacoes_da_Taylor = [
+    " Porque, querido, eu sou um pesadelo vestido como um sonho. (Blank Space) ",
+    " Um amigo de todos é um amigo de ninguém. (Cardigan) ",
+    " Você desenhou estrelas em volta das minhas cicatrizes e agora estou sangrando. (Cardigan) ",
+    " Sim, nós estamos felizes, livres, confusos e sozinhos ao mesmo tempo. (22) ",
+    " Você sabe, os melhores filmes de todos os tempos nunca foram feitos. (the 1) ",
+    " E se você nunca sangrar, você nunca vai crescer. (the 1) ",
+    " Deixe isso tudo para trás e haverá felicidade. (happiness) "
 ];
 
-// Função para exibir uma citação aleatória
-function exibirCitacaoAleatoria() {
-    const randomNumber = Math.floor(Math.random() * taylorSwiftQuotes.length);
-    const quoteElement = document.getElementById("quote");
-    quoteElement.textContent = taylorSwiftQuotes[randomNumber];
+
+function frase_aleatoria() {
+    const sorteio = Math.floor(Math.random() * citacoes_da_Taylor.length);
+    document.getElementById("citacoes_da_Taylor").textContent = citacoes_da_Taylor[sorteio];
 }
 
-// Chame a função para exibir uma citação aleatória quando a página carregar
-window.onload = exibirCitacaoAleatoria;
-
+window.onload = frase_aleatoria;
 
 
 
